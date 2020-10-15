e411488e3a377aaebc32baeba295cb17ed284730
ShawnL <shawnlin0201@gmail.com>
2020-10-15 18:26:32 +0800

    0.1.0 done

---
+++ js/website-list.js
@@ -0,0 +1,386 @@
+var collectionWebsite = [
+    {
+        author: 'ShawnL',
+        url: 'https://github.com/shawnlin0201',
+        tag: ['JavaScript','Cat Lover'],
+        intro: '認識作者一下'
+    },
+    {
+        author: 'sqz777',
+        url: 'https://dotblogs.com.tw/Im_sqz777/2017/09/02/004219',
+        tag: ['變數物件','執行物件'],
+        intro: 'JavaScript的運作原理'
+    },
+    {
+        author: '汤姆大叔',
+        url: 'https://wiki.jikexueyuan.com/project/javascript-depth-understanding/variable-object.html',
+        tag: ['變數物件'],
+        intro: ''
+    },
+    {
+        author: 'Huli',
+        url: 'https://blog.huli.tw/2018/11/10/javascript-hoisting-and-tdz/',
+        tag: ['提升','變數物件','執行物件'],
+        intro: '我知道你懂 hoisting，可是你了解到多深？'
+    },
+    {
+        author: 'yehyeh',
+        url: 'http://notepad.yehyeh.net/Content/WebDesign/Javascript/ECMA/Core/JavaScriptCore.php',
+        tag: ['物件','原型鍊','建構函式','呼叫堆疊','執行上下文','變數物件','執行物件','作用域','閉包','this'],
+        intro: 'ECMA-262 Javascript 核心大全'
+    },
+    {
+        author: 'Kuro',
+        url: 'https://kuro.tw/posts/2017/10/12/What-is-THIS-in-JavaScript-%E4%B8%8A/',
+        tag: ['this'],
+        intro: ''
+    },
+    {
+        author: 'Huli',
+        url: 'https://blog.techbridge.cc/2019/02/23/javascript-this/',
+        tag: ['this'],
+        intro: '淺談 JavaScript 頭號難題 this：絕對不完整，但保證好懂'
+    },
+    {
+        author: '卡斯伯',
+        url: 'https://wcc723.github.io/javascript/2017/12/12/javascript-this/',
+        tag: ['this'],
+        intro: 'JavaScript 的 this 到底是誰？'
+    },
+    {
+        author: 'PJChen',
+        url: 'https://pjchender.blogspot.com/2017/01/es6-arrow-function.html',
+        tag: ['this','箭頭函式'],
+        intro: ''
+    },
+    {
+        author: 'Mike Huang',
+        url: 'https://medium.com/%E9%BA%A5%E5%85%8B%E7%9A%84%E5%8D%8A%E8%B7%AF%E5%87%BA%E5%AE%B6%E7%AD%86%E8%A8%98/%E5%BF%83%E5%BE%97-%E8%AA%8D%E8%AD%98%E5%90%8C%E6%AD%A5%E8%88%87%E9%9D%9E%E5%90%8C%E6%AD%A5-callback-promise-async-await-640ea491ea64',
+        tag: ['同步 & 非同步','回呼函式','Promise','Asymc/Await'],
+        intro: '認識同步與非同步'
+    },
+    {
+        author: 'Huli',
+        url: 'https://blog.huli.tw/2019/10/04/javascript-async-sync-and-callback/#sync',
+        tag: ['同步 & 非同步','回呼函式', 'Asymc/Await'],
+        intro: ''
+    },
+    {
+        author: 'keifergu',
+        url: 'https://juejin.im/entry/6844903471280291854',
+        tag: ['微觀任務','宏觀任務'],
+        intro: '理解 JavaScript 中的 macrotask 和 microtask'
+    },
+    {
+        author: 'Mooji',
+        url: 'https://ithelp.ithome.com.tw/articles/10222737',
+        tag: ['微觀任務','宏觀任務'],
+        intro: ' Macrotask 與 MicroTask'
+    },
+    {
+        author: '程式前沿',
+        url: 'https://codertw.com/ios/21026/',
+        tag: ['微觀任務','宏觀任務'],
+        intro: '簡要總結microtask和macrotask'
+    },
+    {
+        author: 'PJChen',
+        url: 'https://pjchender.blogspot.com/2017/08/javascript-learn-event-loop-stack-queue.html',
+        tag: ['呼叫堆疊','事件循環','任務隊列'],
+        intro: '理解 JavaScript 中的事件循環、堆疊、佇列和併發模式'
+    },
+    {
+        author: '大俊',
+        url: 'https://darjun.github.io/2018/11/23/javascript-callstack-eventloop/',
+        tag: ['呼叫堆疊','事件循環','任務隊列'],
+        intro: '深入理解Javascript之CallStack&EventLoop'
+    },
+    {
+        author: 'Philip Roberts',
+        url: 'https://www.youtube.com/watch?v=8aGhZQkoFbQ&feature=emb_title',
+        tag: ['呼叫堆疊','事件循環','任務隊列'],
+        intro: '所以說event loop到底是什麼玩意兒？'
+    },
+    {
+        author: 'MDN',
+        url: 'https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/EventLoop',
+        tag: ['呼叫堆疊','事件循環','任務隊列'],
+        intro: '並行模型和事件循環'
+    },
+    {
+        author: 'Mooji',
+        url: 'https://ithelp.ithome.com.tw/articles/10221944?sc=iThelpR',
+        tag: ['呼叫堆疊','事件循環','任務隊列'],
+        intro: 'Event Queue & Event Loop 、Event Table'
+    },
+    {
+        author: ' JavaScript中Scope Chain和outer environment的概念',
+        url: 'https://pjchender.blogspot.com/2015/12/javascriptscope-chainouter-environment.html',
+        tag: ['作用域'],
+        intro: 'JavaScript中Scope Chain和outer environment的概念'
+    },
+    {
+        author: 'Huli',
+        url: 'https://blog.techbridge.cc/2018/12/08/javascript-closure/#scope',
+        tag: ['作用域','閉包'],
+        intro: '所有的函式都是閉包：談 JS 中的作用域與 Closure'
+    },
+    {
+        author: '编程go',
+        url: 'https://www.jianshu.com/p/458930ff4778',
+        tag: ['執行期'],
+        intro: 'Javascript是如何工作的'
+    },
+    {
+        author: 'Jamie Uttariello',
+        url: 'https://medium.com/@olinations/the-javascript-runtime-environment-d58fa2e60dd0',
+        tag: ['執行期'],
+        intro: 'The Javascript Runtime Environment'
+    },
+    {
+        author: 'Meggin Kearney',
+        url: 'https://developers.google.com/web/tools/chrome-devtools/memory-problems/heap-snapshots?hl=zh-tw',
+        tag: ['記憶體管理機制'],
+        intro: '如何記錄堆快照'
+    },
+    {
+        author: 'Kayce Basques',
+        url: 'https://developers.google.com/web/tools/chrome-devtools/memory-problems?hl=zh-tw',
+        tag: ['記憶體管理機制'],
+        intro: '解決記憶體問題'
+    },
+    {
+        author: '阮一峰',
+        url: 'http://www.ruanyifeng.com/blog/2017/04/memory-leak.html',
+        tag: ['記憶體管理機制'],
+        intro: 'JavaScript 記憶體泄漏教程'
+    },
+    {
+        author: 'MDN',
+        url: 'https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Memory_Management',
+        tag: ['記憶體管理機制'],
+        intro: '記憶體管理'
+    },
+    {
+        author: 'Allan Sun',
+        url: 'https://blog.cssuen.tw/memory-management-in-javascript-1e486a1cd182',
+        tag: ['記憶體管理機制'],
+        intro: 'Memory Management in JavaScript'
+    },
+    {
+        author: 'Alexander Zlatkov',
+        url: 'https://blog.sessionstack.com/how-javascript-works-memory-management-how-to-handle-4-common-memory-leaks-3f28b94cfbec',
+        tag: ['記憶體管理機制'],
+        intro: 'memory management + how to handle 4 common memory leaks'
+    },
+    {
+        author: 'bigboys.me',
+        url: 'https://medium.com/@bigboys.me/%E6%99%AE%E9%80%9A%E9%A1%9E%E5%9E%8B%E5%92%8C%E5%B0%8D%E8%B1%A1%E7%9A%84%E5%8D%80%E5%88%A5-%E6%A3%A7%E5%85%A7%E5%AD%98-stack-%E5%A0%86%E5%85%A7%E5%AD%98-heap-44295724848c',
+        tag: ['記憶體管理機制'],
+        intro: '了解瀏覽器的棧內存 (Stack) ＆ 堆內存 (Heap)'
+    },
+    {
+        author: 'Ethan Nam',
+        url: 'https://medium.com/@ethannam/javascripts-memory-model-7c972cd2c239',
+        tag: ['記憶體管理機制'],
+        intro: 'JavaScript’s Memory Model'
+    },
+    {
+        author: 'Deepu K Sasidharan',
+        url: 'https://deepu.tech/memory-management-in-programming/',
+        tag: ['記憶體管理機制'],
+        intro: 'Demystifying memory management in modern programming languages'
+    },
+    {
+        author: 'Deepu K Sasidharan',
+        url: 'https://deepu.tech/memory-management-in-v8/',
+        tag: ['記憶體管理機制'],
+        intro: 'Visualizing memory management in V8 Engine'
+    },
+    {
+        author: 'Huli',
+        url: 'https://blog.techbridge.cc/2020/04/18/javascript-function-is-awesome-and-i-love-it/',
+        tag: ['函式'],
+        intro: '覺得 JavaScript function 很有趣的我是不是很奇怪'
+    },
+    {
+        author: '程式前沿',
+        url: 'https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/666367/#outline__1',
+        tag: ['執行上下文'],
+        intro: '深入理解Javascript之ExecutionContext'
+    },
+    {
+        author: 'Gemahar Rafi',
+        url: 'https://dev.to/gemhar/execution-context-the-secret-life-of-functions-1bl1',
+        tag: ['執行上下文'],
+        intro: 'Execution Context & the Secret Life of Functions'
+    },
+    {
+        author: 'Sukhjinder Arora',
+        url: 'https://blog.bitsrc.io/understanding-execution-context-and-execution-stack-in-javascript-1c9ea8642dd0',
+        tag: ['執行上下文'],
+        intro: 'Understanding Execution Context and Execution Stack in Javascript'
+    },
+    {
+        author: 'Schaos',
+        url: 'https://medium.com/schaoss-blog/%E5%89%8D%E7%AB%AF%E4%B8%89%E5%8D%81-09-js-%E4%BB%80%E9%BA%BC%E6%98%AF%E9%96%89%E5%8C%85-16cd2085f5ed',
+        tag: ['閉包'],
+        intro: '什麼是閉包？'
+    },
+    {
+        author: 'shubo',
+        url: 'https://shubo.io/javascript-closure/',
+        tag: ['閉包','函式'],
+        intro: 'avaScript Closure (閉包)、函式與語彙環境'
+    },
+    {
+        author: 'Dmitry Soshnikov',
+        url: 'http://dmitrysoshnikov.com/ecmascript/chapter-6-closures/',
+        tag: ['閉包'],
+        intro: 'ECMA-262-3 in detail. Chapter 6. Closures.'
+    },
+    {
+        author: 'Kuro',
+        url: 'https://ithelp.ithome.com.tw/articles/10193009',
+        tag: ['閉包'],
+        intro: '閉包 Closure'
+    },
+    {
+        author: 'Fooish',
+        url: 'https://www.fooish.com/javascript/function-closure.html',
+        tag: ['閉包'],
+        intro: 'JavaScript Function Closure (閉包)'
+    },
+    {
+        author: 'Richaaaard',
+        url: 'https://iter01.com/117049.html',
+        tag: ['閉包'],
+        intro: '什麼是JavaScript閉包終極全解'
+    },
+    {
+        author: 'whatthefuckis',
+        url: 'https://whatthefuck.is/closure',
+        tag: ['閉包'],
+        intro: 'a closure'
+    },
+    {
+        author: 'eyesofkids',
+        url: 'https://eyesofkids.gitbooks.io/javascript-start-from-es6/content/part4/callback.html',
+        tag: ['回呼函式'],
+        intro: ''
+    },
+    {
+        author: '微中子',
+        url: 'https://ithelp.ithome.com.tw/articles/10191579',
+        tag: ['執行期'],
+        intro: '深入探討瀏覽器引擎如何進行解析'
+    },
+    {
+        author: 'Tali Garsiel and Paul Irish',
+        url: 'https://www.html5rocks.com/zh/tutorials/internals/howbrowserswork/',
+        tag: ['執行期'],
+        intro: '浏览器的工作原理：新式网络浏览器幕后揭秘'
+    },
+    {
+        author: '龚麒',
+        url: 'https://zhuanlan.zhihu.com/p/32189701',
+        tag: ['抽象語法樹'],
+        intro: 'AST in Modern JavaScript'
+    },
+    {
+        author: '刘羽冲',
+        url: 'https://segmentfault.com/a/1190000016231512',
+        tag: ['抽象語法樹'],
+        intro: 'AST抽象语法树——最基础的javascript重点知识，99%的人根本不了解'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10241813',
+        tag: ['執行期'],
+        intro: '瀏覽器是如何看懂我寫的程式？'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10241813',
+        tag: ['執行期'],
+        intro: '瀏覽器是如何看懂我寫的程式？'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10242565',
+        tag: ['變數'],
+        intro: '變數（Variable）的含意'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10243152',
+        tag: ['資料型別','原始物件','非原始物件'],
+        intro: '資料型別（data types）之間的差異'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10243729',
+        tag: ['運算','運算子'],
+        intro: '運算式（Expression）與運算子（Operators）'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10246308',
+        tag: ['函式'],
+        intro: '函式（function）'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10248510',
+        tag: ['瀏覽器','瀏覽器物件模型'],
+        intro: '瀏覽器中的 JavaScript'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10248722',
+        tag: ['瀏覽器','瀏覽器物件模型'],
+        intro: '瀏覽器中的文件操作'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10249178',
+        tag: ['瀏覽器','瀏覽器事件'],
+        intro: '瀏覽器中的事件處理'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10250374',
+        tag: ['瀏覽器','資源請求'],
+        intro: '瀏覽器中的資源請求與響應'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10251283',
+        tag: ['瀏覽器','儲存機制'],
+        intro: '瀏覽器中的儲存機制'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10253457',
+        tag: ['執行上下文'],
+        intro: '執行上下文（Execution Context）'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10251993',
+        tag: ['記憶體管理機制'],
+        intro: '瀏覽器中的回收機制'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://shawnlin0201.github.io/JavaScript/JavaScript-Array-methods/',
+        tag: ['迴圈'],
+        intro: '陣列處理迴圈的方法（forEach、map、filter、reduce）'
+    },
+    {
+        author: 'ShawnL',
+        url: 'https://ithelp.ithome.com.tw/articles/10245112',
+        tag: ['流程控制'],
+        intro: '流程與計算：敘述句'
+    },
+]
